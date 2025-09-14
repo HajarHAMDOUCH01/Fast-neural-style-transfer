48d7cad9d4134584a1e3e32d4e2ca3940cf91f42
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2025-09-14 19:22:56 +0100

    small fix

--- onnx_version/onnx_model.py
+++ onnx_version/onnx_model.py
@@ -4,8 +4,12 @@ import torch.nn.functional as F
 
 from models.model import StyleTransferNet
 
+import sys
+
+sys.path.append('/content/real-time-neural-style-transfer')
+
 model = StyleTransferNet().eval()
 dummy_input = (torch.randn(1,3,256,256))
 onnx_program = torch.onnx.export(model, dummy_input, dynamo=True)
 
-onnx_program.save("nst_model.onnx")
+onnx_program.save("/content/nst_model.onnx")
