8850c3e472b3b25f3e0e2d5ce6172ab066b9bec6
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2025-09-14 20:30:59 +0100

    onnx and onnx inference

--- onnx_version/onnx_inference.py
+++ onnx_version/onnx_inference.py
@@ -1,3 +1,37 @@
 import onnx 
+import torchvision.transforms as transforms
+from PIL import Image
+import torch
+import onnxruntime
+import numpy as np
 
-onnx_model = onnx.load("onn")
+onnx_model = onnx.load("")
+onnx.checker.check_model(onnx_model)
+
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),  
+    transforms.ToTensor(),
+])
+
+def from_img_to_tensor(image_path):
+    image = Image.open(image_path).to("RGB")
+    image_tensor = transform(image).unsqueeze(0)
+    return image_tensor
+
+def fom_tensor_to_image(image_tensor):
+    if image_tensor.dim == 4:
+        image_tensor = image_tensor[0]
+    image_tensor = torch.clamp(image_tensor, 0, 1)
+    image = transforms.ToPILImage()(image_tensor.cpu())
+    return image
+
+def onnx_inference(image_path, output_dir):
+    input_tensor = from_img_to_tensor(image_path)
+    onnx_input = [tensor.numpy(force=True) for tensor in input_tensor]
+    ort_session = onnxruntime.InferenceSession(
+        "onnx_file.onnx", providers=["CPUExecutionProvider"]
+    )
+    onnxruntime_input = {input_arg.name: input_value for input_arg, input_value in zip(ort_session.get_inputs(), onnx_input)}
+    onnxruntime_output = ort_session.run(None, onnxruntime_input)[0]
+    output_image = fom_tensor_to_image(onnxruntime_output)
+    output_image.save(f"{output_dir}/ouput_image.jpg")
--- onnx_version/onnx_model.py
+++ onnx_version/onnx_model.py
@@ -10,13 +10,18 @@ sys.path.append('/content/real-time-neural-style-transfer')
 
 model = StyleTransferNet()
 
-model_state_dict = torch.load("model.pth", map_location="cpu")
+model_state_dict = torch.load("", map_location="cpu")
 model.load_state_dict(model_state_dict)
 model.eval()
 
 dummy_input = (torch.randn(1,3,256,256))
-torch.onnx.export(model, dummy_input, dynamo=True)
+torch.onnx.export(model, 
+                dummy_input,
+                "/content/nst_onnx_model.onnx",
+                export_params=True,
+                opset_version=17,
+                input_names=["input_image"],
+                output_names=["output_image"],
+                dynamo=True)
 
-onnx_program.save("/content/nst_model.onnx")
-
-
+print("ONNX model saved at /content/nst_model.onnx")
