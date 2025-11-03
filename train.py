adb7dfd24eb1ff7f54e9b98dd0e0f2c65d651347
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2025-11-03 19:30:17 +0100

    commit

--- train.py
+++ train.py
@@ -265,7 +265,7 @@ def train_style_transfer(
 
             # # changing of weights every 10000
             if total_iterations % 1000 == 0:
-                style_weight = style_weight * 1.3      
+                style_weight = style_weight * 1.5   
             if total_iterations % 10000 == 0: 
                 content_weight = content_weight / 1.05
        
